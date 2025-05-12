230db105b3614ee1b036570eb2a425b62f47b41b
akhilaq3 <akhilaq3@gmail.com>
2025-05-12 21:24:21 +0530

    reversing a string in various ways

---
+++ reversestring.py
@@ -0,0 +1,31 @@
+#reversing a string using slice operator
+# s="akhila"
+# print(s[::-1])
+
+
+
+#reversing a string using reversed function
+# s="lahari"
+# r=reversed(s)
+# res=''.join(r)
+# print(res)
+
+
+
+#reversing the string without using inbuilt functions
+# s="akhila"
+# n=len(s)
+# i=n-1
+# rev=""
+# while i>=0:
+#     rev+=s[i]
+#     i-=1
+# print(rev)
+
+
+#reversing the words of the sentence
+# s="python interpreted language"
+# l=s.split()
+# l1=l[::-1]
+# print(l1)
+
