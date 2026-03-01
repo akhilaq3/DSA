2ef39548cd7f15c486bc2ee38598ed8b0a958a99
Vanam Akhila <akhilaq3@gmail.com>
2026-03-01 11:47:42 +0530

    gcd of two numbers
    loop stops at b=0 and value of a is GCD

---
+++ gcd.py
@@ -0,0 +1,11 @@
+# num1,num2=map(int,input().split())
+# while num2>0:
+#     rem=num1%num2           # consider 6,8          rem=6%8=6   rem=8%6=2   rem=6%2=0
+#     num1=num2                   #num1=6             num1=8      num1=6      gcd=num1=6
+# gcd=num1                         #num2=8            num2=6      num2=2
+# print(gcd)                  #factors of 6 is 1,2,3,6
+                            #factors of 8 is 1,2,4,8    greatest common factor is 2
+
+
+
+
